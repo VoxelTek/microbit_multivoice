e919002a6ba046fe0612db64bf3b0ea899a8a2c2
VoxelTek <jaolco2008@gmail.com>
2020-10-21 17:33:46 +1000

    Removed unnessecary code

--- MultiVoice.py
+++ MultiVoice.py
@@ -3,20 +3,17 @@ from microbit import *
 import music
 
 #Initialises the variables and lists.
-freqs = []
+freqs = [55, 0, 77, 0, 0, 0]
 freqlength = 0
 
 
 while True:
     #Plays the frequencies.
     for length in range(len(freqs)): # Repeats for all items in the list.
-        freqlength = 0
         if freqlength > 9: #Checks to see if the list has too many notes in it.
-            length = len(freqs)
+            freqlength = 0 #Sets the freqlength variable back to 0.
+            length = len(freqs) #Starts the for loop over again, from the beginning.
         else:
             if freqs[length] != 0: #Checks if the current item in freqs is 0. If so, skip.
                 freqlength = freqlength + 1
-                if freqs[length] != -1: # Checks if the current note is a rest or not.
-                    music.pitch(freqs[length], 10) #Plays a normal note, from the current pitch selected from the "freqs" list.
-                else:
-                    music.pitch(0, 10) #Plays an empty note, aka a rest.
+                music.pitch(freqs[length], 10) #Plays a normal note, from the current pitch selected from the "freqs" list.
