10991befb85ab7dca9703233a53a8d5d857bf549
VoxelTek <jaolco2008@gmail.com>
2020-10-27 19:10:26 +1000

    Added some functions and lists.
    Added the "notecheck" function, added some required lists.

--- MultiVoice.py
+++ MultiVoice.py
@@ -3,10 +3,17 @@ from microbit import *
 import music
 
 #Initialises the variables and lists.
-freqs = []
-freqlength = 0
+freqs = [] #Stores the currently playing frequencies.
+
+freqlength = 0 #Used as a temp variable for if there are too many voices
+
+notelist = []
+notestart = []
+noteend = []
+
 
 while True:
+    notecheck()
     #Plays the frequencies.
     for length in range(len(freqs)): # Repeats for all items in the list.
         if freqlength > 9: #Checks to see if the list has too many notes in it.
@@ -16,3 +23,6 @@ while True:
             if freqs[length] != 0: #Checks if the current item in freqs is 0. If so, skip.
                 freqlength = freqlength + 1
                 music.pitch(freqs[length], 10) #Plays a normal note, from the current pitch selected from the "freqs" list.
+
+
+def notecheck():
