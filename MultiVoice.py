53312f5910a3197a5b44c2c054d7eb53125f62e1
VoxelTek <jaolco2008@gmail.com>
2020-10-28 19:29:11 +1000

    Finished the "notecheck()" function
    Added some lists, variables, and finished the "notecheck()". The program is now semi-complete.

--- MultiVoice.py
+++ MultiVoice.py
@@ -5,15 +5,22 @@ import music
 #Initialises the variables and lists.
 freqs = [] #Stores the currently playing frequencies.
 
-freqlength = 0 #Used as a temp variable for if there are too many voices
+freqlength = 0 #Used as a temp variable for if there are too many voices.
 
-time = 0
+time = 0 #Stores the current time in milliseconds.
 
-notelist = []
-notestart = []
-noteend = []
-notewait = []
-notewait-loc = []
+current-note = 0 #Stores the note that the program is currently at.
+
+temp-loc = 0 #Temporary variable for storing the current position the program is at.
+temp-pos = 0
+
+notelist = [] #List of the notes that will be played.
+notestart = [] #Records when the notes start.
+noteend = [] #Records when the notes end.
+
+
+notewait = [] #Remembers what notes are currently playing.
+notewait-loc = [] #Remembers the position of the currently playing notes in "freqs".
 
 
 while True:
@@ -32,4 +39,13 @@ while True:
 def notecheck():
     for notelength in range(len(notewait)):
         if noteend(notewait) == time or noteend(notewait) > time:
-            
+            del freqs[notewait-loc[notelength]]
+            del notewait-loc[notelength]
+            del notewait[notelength]
+    for loop in range(len(notelist) - current-note):
+        temp-loc = - + loop
+        temp-pos = -1
+        for frequencylength in range(len(freqs)):
+            if freqs[frequencylength] == 0:
+                temp-pos = frequencylength
+        insert(temp-pos)
